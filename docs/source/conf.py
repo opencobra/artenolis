14c520ca211c31517a209485be980070b8d982c7
Sylvain Arreckx <sylvain.arreckx@gmail.com>
2018-01-26 14:48:55 +0100

    add favicon

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -171,7 +171,7 @@ html_logo = '_static/img/logo_artenolis.png'
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
 #
-# html_favicon = None
+html_favicon = '_static/img/logo_artenolis.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
