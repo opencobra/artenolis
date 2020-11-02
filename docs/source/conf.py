91065f54cfc6a7f399af1465c5da660fe92914bd
Joe Desbonnet <jdesbonnet@gmail.com>
2020-11-02 03:13:16 +0000

    change from sphinx_julia_theme to sphinx_cobra_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -123,15 +123,15 @@ todo_include_todos = True
 
 # -- Options for HTML output ----------------------------------------------
 
-html_theme = "sphinx_julia_theme"
+html_theme = "sphinx_cobra_theme"
 
-import sphinx_julia_theme
+import sphinx_cobra_theme
 import sphinx_rtd_theme
 
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path(),
-                   sphinx_julia_theme.get_theme_dir()]
+                   sphinx_cobra_theme.get_theme_dir()]
 print html_theme_path
-html_sidebars = sphinx_julia_theme.default_sidebars()
+html_sidebars = sphinx_cobra_theme.default_sidebars()
 
 # shutil.copy()
 
